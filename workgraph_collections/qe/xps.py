91e66b4e366b6ea55bce3708a2ad89ba372c1707
superstar54 <xingwang1991@gmail.com>
2024-06-10 06:32:49 +0200

    update qe xps

--- workgraph_collections/qe/xps.py
+++ workgraph_collections/qe/xps.py
@@ -1,11 +1,26 @@
-from aiida_workgraph import WorkGraph, node
-from aiida.orm import StructureData, Dict, KpointsData, Code
-from workgraph_collections.common.xps import get_marked_structures, binding_energy
+from aiida_workgraph import WorkGraph, node, build_node
+from aiida.orm import StructureData, Dict, KpointsData, Code, List, Bool
+from workgraph_collections.common.xps import binding_energy
+from aiida_quantumespresso.workflows.functions.get_xspectra_structures import (
+    get_xspectra_structures,
+)
+from aiida_quantumespresso.workflows.functions.get_marked_structures import (
+    get_marked_structures,
+)
+
+GetXspectraStructureNode = build_node(
+    get_xspectra_structures,
+    outputs=[["General", "output_parameters"]],
+)
+GetMarkedStructuresNode = build_node(
+    get_marked_structures,
+    outputs=[["General", "output_parameters"]],
+)
 
 
 @node.graph_builder(outputs=[["context.scf", "result"]])
 def run_scf(
-    structures: StructureData = None,
+    structure: StructureData = None,
     code: Code = None,
     parameters: dict = None,
     kpoints: KpointsData = None,
@@ -14,14 +29,23 @@ def run_scf(
     core_hole_treatment: str = "xch",
     is_molecule: bool = None,
     metadata: dict = None,
+    **marked_structures,
 ):
     from aiida_workgraph import WorkGraph
     from aiida_quantumespresso.calculations.pw import PwCalculation
     from copy import deepcopy
 
     #
-    wg = WorkGraph("run_scf")
+    output_parameters = marked_structures.pop("output_parameters", Dict({})).get_dict()
+    sites_info = output_parameters["equivalent_sites_data"]
+    print("sites_info", sites_info)
+
+    for site in sites_info:
+        abs_element = sites_info[site]["symbol"]
+        pseudos[abs_element] = core_hole_pseudos["gipaw"][abs_element]
     # ground state
+    wg = WorkGraph("run_scf")
+    supercell = marked_structures.pop("supercell", structure)
     pw_ground = wg.nodes.new(PwCalculation, name="ground")
     pw_ground.set(
         {
@@ -30,17 +54,20 @@ def run_scf(
             "kpoints": kpoints,
             "pseudos": pseudos,
             "metadata": metadata,
-            "structure": structures.pop("ground"),
+            "structure": supercell,
         }
     )
     pw_ground.to_context = [["output_parameters", "scf.ground"]]
+    # remove unwanted data
+    for site in ["standardized_structure"]:
+        marked_structures.pop(site, None)
     # excited state node
-    for key, structure in structures.items():
+    for key, marked_structure in marked_structures.items():
         pseudos1 = pseudos.copy()
-        peak = structure.base.extras.get("info")["peak"]
-        pseudos1["X"] = core_hole_pseudos[peak]
+        symbol = sites_info[key]["symbol"]
+        pseudos1["X"] = core_hole_pseudos["core_hole"][symbol]
         # remove pseudo of non-exist element
-        pseudos1 = {kind.name: pseudos1[kind.name] for kind in structure.kinds}
+        pseudos1 = {kind.name: pseudos1[kind.name] for kind in marked_structure.kinds}
         # update parameters
         ch_parameters = deepcopy(parameters)
         if is_molecule:
@@ -83,7 +110,7 @@ def run_scf(
                 "kpoints": kpoints,
                 "pseudos": pseudos1,
                 "metadata": metadata,
-                "structure": structure,
+                "structure": marked_structure,
                 "settings": settings,
             }
         )
@@ -96,6 +123,7 @@ def xps_workgraph(
     structure: StructureData = None,
     code: Code = None,
     atoms_list: list = None,
+    element_list: list = None,
     parameters: dict = None,
     kpoints: KpointsData = None,
     pseudos: dict = None,
@@ -111,16 +139,27 @@ def xps_workgraph(
     3. Calculate the binding energy.
     """
     wg = WorkGraph()
-    get_marked_structures1 = wg.nodes.new(
-        get_marked_structures,
-        name="get_marked_structures",
-        marker="X",
-        structure=structure,
-        atoms_list=atoms_list,
-    )
+    if atoms_list:
+        structures_node = wg.nodes.new(
+            GetMarkedStructuresNode,
+            name="marked_structures",
+            structure=structure,
+            atoms_list=atoms_list,
+        )
+    else:
+        structures_node = wg.nodes.new(
+            GetXspectraStructureNode,
+            name="marked_structures",
+            structure=structure,
+            kwargs={
+                "absorbing_elements_list": List(element_list),
+                "is_molecule_input": Bool(is_molecule),
+            },
+        )
     run_scf1 = wg.nodes.new(
         run_scf,
         name="run_scf",
+        structure=structure,
         code=code,
         parameters=parameters,
         kpoints=kpoints,
@@ -129,13 +168,14 @@ def xps_workgraph(
         is_molecule=is_molecule,
         core_hole_treatment=core_hole_treatment,
         metadata=metadata,
-        structures=get_marked_structures1.outputs["structures"],
+        marked_structures=structures_node.outputs["_outputs"],
     )
     wg.nodes.new(
         binding_energy,
         name="binding_energy",
-        corrections=correction_energies,
+        sites_info=structures_node.outputs["output_parameters"],
         scf_outputs=run_scf1.outputs["result"],
-        energy_units="eV",
+        corrections=correction_energies,
+        energy_units="a.u",
     )
     return wg
